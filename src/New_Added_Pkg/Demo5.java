93a442eab8748a03faed3de31d5a09043c3f7c37
Shivalika20 <shivalikav2005@gmail.com>
2020-04-24 00:39:05 +0530

    Added new package

---
+++ src/New_Added_Pkg/Demo5.java
@@ -0,0 +1,11 @@
+package New_Added_Pkg;
+
+public class Demo5 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is Demo5");
+
+	}
+
+}
---
+++ src/New_Added_Pkg/Demo6.java
@@ -0,0 +1,11 @@
+package New_Added_Pkg;
+
+public class Demo6 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is DEmo6");
+
+	}
+
+}
